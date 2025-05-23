78eb0f23b1b172166446fb1b50c57da2995728c4
sedangmanusia <qurrotunafina11@gmail.com>
2025-05-23 14:33:14 +0700

    [BAB 9] NOTIFICATIONS

--- src/screens/AddKomunitasForm/index.jsx
+++ src/screens/AddKomunitasForm/index.jsx
@@ -6,6 +6,7 @@ import {useNavigation} from '@react-navigation/native';
 import {fontType, colors} from '../../theme';
 import ImagePicker from 'react-native-image-crop-picker';
 import { addDoc, collection, getFirestore } from '@react-native-firebase/firestore';
+import notifee, {AndroidImportance} from '@notifee/react-native';
 
 // Sample dataCategory array for category options
 const dataCategory = [
@@ -16,6 +17,116 @@ const dataCategory = [
 ];
 
 const AddKomunitasForm = () => {
+  const dataUploadOption = [
+    {id: 1, name: 'Immediate'},
+    {id: 2, name: 'After 10 Seconds'},
+    {id: 3, name: 'After 30 Seconds'},
+];	
+ 
+  const [uploadOption, setUploadOption] = useState({id: 1, name: 'Immediate'});
+
+  const handleDelayedPost = async jeda => {
+const hasil = await notifee.requestPermission();
+    if (hasil.authorizationStatus == 0) {
+      await notifee.openNotificationSettings();
+      return;
+    }
+
+    // tambahkan notifikasi channel
+    const channelId = await notifee.createChannel({
+      id: 'post',
+      name: 'Upload Blog',
+      importance: AndroidImportance.HIGH,
+    });
+
+    // tambahkan Foreground Service
+    notifee.registerForegroundService(notification => {
+      return new Promise(async (resolve) => {
+        // tunda upload blog, sesuai dengan parameter jeda
+        await new Promise(resolveTimeout => {
+          setTimeout(() => {
+            resolveTimeout();
+          }, jeda * 1000);
+        });
+
+        // setelah jeda terjadi baru upload blog
+        let filename = image.substring(image.lastIndexOf('/') + 1);
+        const extension = filename.split('.').pop();
+        const name = filename.split('.').slice(0, -1).join('.');
+        filename = name + Date.now() + '.' + extension;
+
+        try {
+          const imageFormData = new FormData();
+          imageFormData.append('file', {
+            uri: image,
+            type: `image/${extension}`, // or 'image/png'
+            name: filename,
+          });
+
+          const result = await fetch(
+            'https://backend-file-praktikum.vercel.app/upload/',
+            {
+              method: 'POST',
+              body: imageFormData,
+            },
+          );
+          if (result.status !== 200) {
+            throw new Error('failed to upload image');
+          }
+
+          const {url} = await result.json();
+
+          const db = getFirestore();
+          const komunitasRef = collection(db, 'komunitas');
+          await addDoc(komunitasRef, {
+            title: formData.title,
+            description: formData.description,
+            uploadDate: formData.uploadDate,
+            eventDate: formData.eventDate,
+            location: formData.location,
+            quota: Number(formData.quota),
+            image: url,
+            createdAt: new Date(),
+          });
+
+          console.log('Blog added!');
+
+          // tampilkan notifikasi blog sukses di upload
+          notifee.displayNotification({
+            title: 'Kegiatan Komunitas DiUpload👍',
+            body: `Kegiatan Komunitas Sukses di Upload`,
+            android: {
+              channelId: channelId,
+            },
+          });
+        } catch (error) {
+          console.log(error);
+          // tampilkan notifikasi blog gagal di upload
+          notifee.displayNotification({
+            title: 'Kegiatan Komunitas Gagal DiUpload ❗❗',
+            body: `Kegiatan Komunitas Gagal di Upload`,
+            android: {
+              channelId: channelId,
+            },
+          });
+        }
+
+        notifee.stopForegroundService();
+        resolve();
+      });
+    });
+
+    // tampilkan notifikasi blog di upload dengan di jadwalkan
+    notifee.displayNotification({
+      title: 'Upload Kegiatan Komunitas Dijadwalkan',
+      body: `Kegiatan Komunitas akan di Upload dalam ${jeda} detik ⬆️⬆️`,
+      android: {
+        channelId: channelId,
+        asForegroundService: true,
+      },
+    });
+  };
+
   const handleImagePick = async () => {
     ImagePicker.openPicker({
       width: 1920,
@@ -72,49 +183,93 @@ const AddKomunitasForm = () => {
   };
 
   const handleUpload = async () => {
-    let filename = image.substring(image.lastIndexOf('/') + 1);
-    const extension = filename.split('.').pop();
-    const name = filename.split('.').slice(0, -1).join('.');
-    filename = name + Date.now() + '.' + extension;
+    if (uploadOption.id == 1) {
+      let filename = image.substring(image.lastIndexOf('/') + 1);
+      const extension = filename.split('.').pop();
+      const name = filename.split('.').slice(0, -1).join('.');
+      filename = name + Date.now() + '.' + extension;
 
-    setLoading(true);
-    try {
-      const imageFormData = new FormData();
-      imageFormData.append('file', {
-        uri: image,
-        type: `image/${extension}`, // or 'image/png'
-        name: filename,
-      });
+      setLoading(true);
+      try {
+        const imageFormData = new FormData();
+        imageFormData.append('file', {
+          uri: image,
+          type: `image/${extension}`, // or 'image/png'
+          name: filename,
+        });
 
-      const result = await fetch('https://backend-file-praktikum.vercel.app/upload/', {
-        method: 'POST',
-        body: imageFormData,
-      });
-      if (result.status !== 200) {
-        throw new Error("failed to upload image");
+        const result = await fetch(
+          'https://backend-file-praktikum.vercel.app/upload/',
+          {
+            method: 'POST',
+            body: imageFormData,
+          },
+        );
+        if (result.status !== 200) {
+          throw new Error('failed to upload image');
+        }
+
+        const {url} = await result.json();
+
+        const db = getFirestore();
+        const komunitasRef = collection(db, 'komunitas');
+        await addDoc(komunitasRef, {
+          title: formData.title,
+          description: formData.description,
+          uploadDate: formData.uploadDate,
+          eventDate: formData.eventDate,
+          location: formData.location,
+          quota: Number(formData.quota),
+          image: url,
+          createdAt: new Date(),
+        });
+
+        setLoading(false);
+        console.log('Kegiatan Komunitas added!');
+
+        // create notification channel
+        const channelId = await notifee.createChannel({
+          id: 'post',
+          name: 'Upload Kegiatan Komunitas',
+          importance: AndroidImportance.HIGH,
+        });
+
+        // show success notification
+        await notifee.displayNotification({
+          title: 'Kegiatan Komunitas Diupload👍',
+          body: `Kegiatan Komunitas Sukses Diupload`,
+          android: {
+            channelId: channelId,
+          },
+        });
+
+        navigation.goBack();
+      } catch (error) {
+        setLoading(false);
+        console.log(error);
+
+        // create notification channel
+        const channelId = await notifee.createChannel({
+          id: 'post',
+          name: 'Upload Blog',
+          importance: AndroidImportance.HIGH,
+        });
+
+        // show failure notification
+        await notifee.displayNotification({
+          title: 'Kegiatan Komunitas Gagal Diupload ❗❗',
+          body: `Kegiatan Komunitas Gagal Diupload`,
+          android: {
+            channelId: channelId,
+          },
+        });
       }
-
-      const { url } = await result.json();
-
-      const db = getFirestore();
-      const komunitasRef = collection(db, 'komunitas');
-      await addDoc(komunitasRef, {
-        title: formData.title,
-        description: formData.description,
-        uploadDate: formData.uploadDate,
-        eventDate: formData.eventDate,
-        location: formData.location,
-        quota: Number(formData.quota),
-        image: url,
-        createdAt: new Date(),
-      });
-
-      setLoading(false);
-      console.log('Kegiatan Komunitas Ditambahkan!');
+    } else if (uploadOption.id == 2) { // jadwalkan selama 10 detik baru upload
       navigation.goBack();
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
+      handleDelayedPost(10);
+    } else if (uploadOption.id == 3) { // jadwalkan selama 30 detik baru upload
+      navigation.goBack();
+      handleDelayedPost(30);
     }
   };
 
@@ -191,58 +346,86 @@ const AddKomunitasForm = () => {
             style={input.text}
           />
         </View>
-        {image ? (
-          <View style={{position: 'relative'}}>
-            <FastImage
-              style={{width: '100%', height: 127, borderRadius: 5}}
-              source={{
-                uri: image,
-                headers: {Authorization: 'someAuthToken'},
-                priority: FastImage.priority.high,
-              }}
-              resizeMode={FastImage.resizeMode.cover}
-            />
-            <TouchableOpacity
-              style={{
-                position: 'absolute',
-                top: -5,
-                right: -5,
-                backgroundColor: colors.blue(),
-                borderRadius: 25,
-              }}
-              onPress={() => setImage(null)}>
-              <Add
-                size={20}
-                variant="Linear"
-                color={colors.white()}
-                style={{transform: [{rotate: '45deg'}]}}
-              />
-            </TouchableOpacity>
+
+        {/* Opsi Upload Blog */}
+        <View style={[input.borderDashed]}>
+          <Text style={uploadOptionStyle.title}>Upload Option</Text>
+          <View style={uploadOptionStyle.container}>
+            {dataUploadOption.map((item, index) => {
+              const bgColor =
+                item.id === uploadOption.id
+                  ? colors.black()
+                  : colors.grey(0.08);
+              const color =
+                item.id === uploadOption.id ? colors.white() : colors.grey();
+              return (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() =>
+                    setUploadOption({id: item.id, name: item.name})
+                  }
+                  style={[uploadOptionStyle.item, {backgroundColor: bgColor}]}>
+                  <Text style={[uploadOptionStyle.name, {color: color}]}>
+                    {item.name}
+                  </Text>
+                </TouchableOpacity>
+              );
+            })}
           </View>
-        ) : (
-          <TouchableOpacity onPress={handleImagePick}>
-            <View
-              style={[
-                input.borderDashed,
-                {
-                  gap: 10,
-                  paddingVertical: 30,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                },
-              ]}>
-              <AddSquare color={colors.grey(0.6)} variant="Linear" size={42} />
-              <Text
-                style={{
-                  fontFamily: fontType['Pjs-Regular'],
-                  fontSize: 12,
-                  color: colors.grey(0.6),
-                }}>
-                Upload Thumbnail
-              </Text>
-            </View>
+        </View>
+      {/* Add image upload section before closing ScrollView */}
+      {image ? (
+        <View style={{position: 'relative'}}>
+          <FastImage
+            style={{width: '100%', height: 127, borderRadius: 5}}
+            source={{
+              uri: image,
+              headers: {Authorization: 'someAuthToken'},
+              priority: FastImage.priority.high,
+            }}
+            resizeMode={FastImage.resizeMode.cover}
+          />
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              top: -5,
+              right: -5,
+              backgroundColor: colors.blue(),
+              borderRadius: 25,
+            }}
+            onPress={() => setImage(null)}>
+            <Add
+              size={20}
+              variant="Linear"
+              color={colors.white()}
+              style={{transform: [{rotate: '45deg'}]}}
+            />
           </TouchableOpacity>
-        )}
+        </View>
+      ) : (
+        <TouchableOpacity onPress={handleImagePick}>
+          <View
+            style={[
+              input.borderDashed,
+              {
+                gap: 10,
+                paddingVertical: 30,
+                justifyContent: 'center',
+                alignItems: 'center',
+              },
+            ]}>
+            <AddSquare color={colors.grey(0.6)} variant="Linear" size={42} />
+            <Text
+              style={{
+                fontFamily: fontType['Pjs-Regular'],
+                fontSize: 12,
+                color: colors.grey(0.6),
+              }}>
+              Upload Thumbnail
+            </Text>
+          </View>
+        </TouchableOpacity>
+      )}
       </ScrollView>
       <View style={styles.bottomBar}>
         <TouchableOpacity style={styles.button} onPress={handleUpload}>
@@ -255,6 +438,7 @@ const AddKomunitasForm = () => {
         </View>
       )}
     </View>
+
   );
 };
 
@@ -349,3 +533,27 @@ const category = StyleSheet.create({
     fontSize: 12,
   },
 });
+
+const uploadOptionStyle = StyleSheet.create({
+  title: {
+    fontSize: 12,
+    fontFamily: fontType['Pjs-Regular'],
+    color: colors.grey(0.6),
+  },
+  container: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    gap: 10,
+    marginTop: 10,
+  },
+  item: {
+    paddingHorizontal: 14,
+    paddingVertical: 10,
+    borderRadius: 25,
+  },
+  name: {
+    fontSize: 10,
+    fontFamily: fontType['Pjs-Medium'],
+  },
+});
+
